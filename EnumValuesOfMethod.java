bbbe20d4d7cdb4d10b3a2d62a5042941c8bcf57e
MohithKrish74 <87643124+MohithKrish74@users.noreply.github.com>
2021-07-31 12:31:18 +0530

    Add files via upload

---
+++ EnumValuesOfMethod.java
@@ -0,0 +1,27 @@
+package com.keyword;
+
+import java.util.Scanner;
+
+enum Week {SUNDAY(1),MONDAY(2),TUESDAY(3),WEDNESDAY(4),THURSDAY(5),FRIDAY(6),SATURDAY(7);
+int value;
+Week(int value)
+{
+    this.value=value;
+}
+};
+public class EnumValuesOfMethod
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the Day: ");
+        String day = in.next();
+        for(Week week : Week.values())
+        {
+            if(week==Week.valueOf(day))
+            {
+                System.out.println(week+" has value "+week.value);
+            }
+        }
+    }
+}
